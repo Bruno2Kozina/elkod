2c31e314768debc9abfd35a87f34b7541b9009a6
Mac <brunokozina24@gmail.com>
2025-10-30 10:37:52 +0100

    finish

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,21 +39,6 @@ export default function RootLayout({
 				<title>Elkod</title>
 				<link rel="icon" href="/fa.ico" /> {/* Dodat favicon umjesto fa TODO !!!! */}
 				{/* Place GTM container snippet directly in the head */}
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `
-          (function(w,d,s,l,i){
-            w[l]=w[l]||[];
-            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-            var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-            j.async=true;
-            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-            f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-PR8P244L');
-        `
-					}}
-				/>
 			</head>
 			<body>
 				{/* Place GTM noscript iframe directly in the body */}
